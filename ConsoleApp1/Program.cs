06f8bf56b3c9e7f537819e6e5276b55ea760a5f6
jmike <jmike@DESKTOP-54L68HD>
2017-09-03 10:27:57 -0700

    Added Database and DinnerRepository Class

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            while (true)
+            {
+                DateTime time = System.DateTime.Now;
+                if (time < System.DateTime.Now)
+                {
+                   
+                    Console.WriteLine(time);
+                }
+            }
+                
+            
+        }
+    }
+}
---
+++ RSVP.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[RsvpID] INT NOT NULL PRIMARY KEY, 
+    [DinnerID] INT NOT NULL, 
+    [AttendeeName] NVARCHAR(30) NOT NULL
+	
+)
