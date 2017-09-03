7423570b8015d39e9eb095c524c07c03a6478e9e
jmike <jmike@DESKTOP-54L68HD>
2017-09-03 14:51:49 -0700

    Updated Dinner Class

---
+++ NerdDinner/Models/Dinner.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Web;
+
+namespace NerdDinner.Models
+{
+    public partial class Dinner
+    {
+        public bool IsValid
+        {
+            get { return (GetRuleViolations().Count() == 0); }
+        }
+
+        public IEnumerable<RuleViolation> GetRuleViolations()
+        {
+            if (String.IsNullOrEmpty(Title))
+            {
+                yield return new RuleViolation("Title required", "Title");
+            }
+            if (String.IsNullOrEmpty(Description))
+            {
+                yield return new RuleViolation("Description Required", "Description");
+            }
+            if (String.IsNullOrEmpty(Host))
+            {
+                yield return new RuleViolation("Host Required", "Host");
+            }
+            if (String.IsNullOrEmpty(Address))
+            {
+                yield return new RuleViolation("Address Required", "Address");
+            }
+            if (String.IsNullOrEmpty(Country))
+            {
+                yield return new RuleViolation("Country Required", "Country");
+            }
+            if (String.IsNullOrEmpty(ContactPhone))
+            {
+                yield return new RuleViolation("Phone# Required", "ContactPhone");
+            }
+            if (!PhoneValidator.IsValidNumber(ContactPhone, Country))
+            {
+                yield return new RuleViolation("Phone# does not match country", "ContactPhone");
+            }
+
+            yield break;
+        }
+
+        partial void OnValidate(ChangeAction action)
+        {
+            if (!IsValid)
+            {
+                throw new ApplicationException("Rule violations prevent saving");
+            }
+        }
+    }
+
+    public class RuleViolation
+    {
+        public string ErrorMessage { get; private set; }
+        public string PropertyName { get; private set; }
+
+        public RuleViolation(string errorMessage, string propertyName)
+        {
+            ErrorMessage = errorMessage;
+            PropertyName = propertyName;
+        }
+    }
+}
--- NerdDinner/Models/DinnerRepository.cs
+++ NerdDinner/Models/DinnerRepository.cs
@@ -7,5 +7,53 @@ namespace NerdDinner.Models
 {
     public class DinnerRepository
     {
+        private NerdDinnerDataContext db = new NerdDinnerDataContext();
+
+
+        //Query Methods
+
+        public IQueryable<Dinner> FindAllDinners()
+        {
+            return db.Dinners;
+        }
+
+
+        // Retrieves all dinners after the the current date & time
+        public IQueryable<Dinner> FindUpcomingDinners()
+        {
+            return from dinner in db.Dinners
+                   where dinner.EventDate < DateTime.Now
+                   orderby dinner.EventDate
+                   select dinner;
+        }
+
+
+        //retrieves a specific dinner based on its DinnerID
+        public Dinner GetDinner(int id)
+        {
+            return db.Dinners.SingleOrDefault(d => d.DinnerID == id);
+        }
+
+        // insert and delete
+
+            //Creates a dinner
+        public void Add(Dinner dinner)
+        {
+            db.Dinners.InsertOnSubmit(dinner);
+        }
+
+        //Deletes a dinner and all associated RSVPs
+        public void Delete(Dinner dinner)
+        {
+            db.Dinners.DeleteOnSubmit(dinner);
+            db.Rsvps.DeleteAllOnSubmit(dinner.Rsvps);
+        }
+
+        //Persistance
+
+        public void Save()
+        {
+            db.SubmitChanges();
+        }
     }
 }
